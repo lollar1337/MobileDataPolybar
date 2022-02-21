41c1e88dd69b7afba8a976a2c7b14ec53d6487a5
/etc/Jade <77240684+thisjade@users.noreply.github.com>
2022-02-21 18:34:41 +0100

    Optimizations
    > less calling the ModemManager for Information
    > removing sed
    > better readability

--- mobile-data.sh
+++ mobile-data.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+set -eou pipefail
 
 INTERVAL=1
+SYMBOL=""
 
 while true; do
-	DATA_TYPE=$(mmcli -m ANY -J | jq '.modem.generic."access-technologies"[0]' | sed 's/"//' | sed 's/"//' | tr '[a-z]' '[A-Z]')
-	OPERATOR_NAME=$(mmcli -m ANY -J |jq '.modem."3gpp"."operator-name"' | sed 's/"//' | sed 's/"//')
-	MODEM_STATE=$(mmcli -m ANY -J |jq '.modem.generic.state' | sed 's/"//' | sed 's/"//')
-	SIGNAL_QUALITY=$(mmcli -m ANY -J |jq '.modem.generic."signal-quality".value' | sed 's/"//' | sed 's/"//')
-
-	if [ $MODEM_STATE == "connected" ]
-	then
-		echo "" $OPERATOR_NAME $DATA_TYPE
+	RAW_DATA=$(     mmcli -m ANY -J)
+	DATA_TYPE=$(    jq -r '.modem.generic."access-technologies"[0]' <<< "${RAW_DATA}" | tr '[a-z]' '[A-Z]')
+	MODEM_STATE=$(  jq -r '.modem.generic.state'                    <<< "${RAW_DATA}" )
+	OPERATOR_NAME=$(jq -r '.modem."3gpp"."operator-name"'           <<< "${RAW_DATA}" )
+	
+	if [[ $MODEM_STATE  == "connected" ]]; then
+		echo $SYMBOL $OPERATOR_NAME $DATA_TYPE
 	else
-	        echo "no service"
+		echo "no service"
 	fi
 	sleep $INTERVAL
 done
