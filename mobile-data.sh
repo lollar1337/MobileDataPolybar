315c6f5f25977fed28e87206ca6b6aad5ec15e27
/etc/Jade <77240684+thisjade@users.noreply.github.com>
2022-03-02 09:42:37 +0100

    Added fail handler if ModemManager is not running/does not exist

--- mobile-data.sh
+++ mobile-data.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-set -euo pipefail
+#set -euo pipefail
 
 INTERVAL=1
-SYMBOL="#1"
+SYMBOL=""
 
 while true; do
-	RAW_DATA=$(     mmcli -m ANY -J)
-	DATA_TYPE=$(    jq -r '.modem.generic."access-technologies"[0]' <<< "${RAW_DATA}" | tr '[a-z]' '[A-Z]')
-	MODEM_STATE=$(  jq -r '.modem.generic.state'                    <<< "${RAW_DATA}" )
-	OPERATOR_NAME=$(jq -r '.modem."3gpp"."operator-name"'           <<< "${RAW_DATA}" )
+	RAW_DATA=$(     mmcli -m ANY -J || echo "mmbad" )
 	
-	if [[ $MODEM_STATE  == "connected" ]]; then
-		echo $SYMBOL $OPERATOR_NAME $DATA_TYPE
+	if [[ $RAW_DATA == "mmbad" ]]; then
+		echo ""
 	else
-		echo "no service"
+	
+		DATA_TYPE=$(    jq -r '.modem.generic."access-technologies"[0]' <<< "${RAW_DATA}" | tr '[a-z]' '[A-Z]')
+		MODEM_STATE=$(  jq -r '.modem.generic.state'                    <<< "${RAW_DATA}" )
+		OPERATOR_NAME=$(jq -r '.modem."3gpp"."operator-name"'           <<< "${RAW_DATA}" )
+	
+		if [[ $MODEM_STATE  == "connected" ]]; then
+			echo $SYMBOL $OPERATOR_NAME $DATA_TYPE
+		else
+			echo "disconnected"
+		fi
 	fi
+	
+
 	sleep $INTERVAL
 done
